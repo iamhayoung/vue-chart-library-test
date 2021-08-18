bea871f0168cd916471b7bae10f45c19df96736f
Hayoung Gam <hahhyoung@gmail.com>
2021-08-19 01:48:59 +0900

    Add: ApexCharts Pie Chart 샘플 생성

--- src/components/ApexCharts.vue
+++ src/components/ApexCharts.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
-    <bar-chart></bar-chart>
-    <line-chart></line-chart>
+    <div>
+      <h1>Bar Chart</h1>
+      <bar-chart></bar-chart>
+    </div>
+    <div>
+      <h1>Line Chart</h1>
+      <line-chart></line-chart>
+    </div>
+    <div>
+      <h1>Pie Chart</h1>
+      <pie-chart></pie-chart>
+    </div>
   </div>
 </template>
 
 <script>
 import BarChart from './apexcharts/BarChart.vue';
 import LineChart from './apexcharts/LineChart.vue';
+import PieChart from './apexcharts/PieChart.vue';
 
 export default {
   name: 'ApexCharts',
   components: {
     BarChart,
     LineChart,
+    PieChart,
   },
 };
 </script>
---
+++ src/components/apexcharts/PieChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <apex-chart
+    type="pie"
+    height="350"
+    :options="chartOptions"
+    :series="series"
+  ></apex-chart>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // series: 차트에 들어가는 실질적인 데이터를 담는 곳. 배열로 담아야함.
+      series: [44, 55, 13, 43, 22],
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: 'pie',
+        },
+        labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+        dataLabels: {
+          enabled: true,
+          formatter: function(val) {
+            return `${Math.floor(val)}%`;
+          },
+        },
+        legend: {
+          show: true,
+          position: 'bottom',
+        },
+      },
+    };
+  },
+};
+</script>
